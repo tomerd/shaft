68b2f98b1042d508aff4cec4af36dc36a15f7255
tomer doron <tomer.doron@gmail.com>
2012-02-11 19:14:56 -0500

    commmon extractors

---
+++ server/src/main/scala/org/simbit/shaft/util/Extractors.scala
@@ -0,0 +1,74 @@
+package org.simbit.shaft
+package util
+
+object StringValue 
+{
+	def apply(x:String) = x
+	def unapply(x:AnyRef):Option[String] = x match 
+	{
+		case s:String => Some(s)
+		case _ => None
+	}
+}
+
+object IntValue 
+{
+	def apply(x:Int) = x.toString
+	def unapply(x:AnyRef):Option[Int] = x match 
+	{
+		case s:String => try { Some(s.toInt) } catch { case _ => None }
+		case _ => None
+	}
+}
+
+object ShortValue 
+{
+	def apply(x:Short) = x.toString
+	def unapply(x:AnyRef):Option[Short] = x match 
+	{
+		case s:String => try { Some(s.toShort) } catch { case _ => None }
+		case _ => None
+	}
+}
+
+object LongValue
+{ 
+	def apply(x:Long) = x.toString 
+	def unapply(x:AnyRef):Option[Long] = x match 
+	{ 
+		case s:String => try { Some(s.toLong) } catch { case _ => None } 
+		case _ => None 
+  	}
+}
+
+object FloatValue 
+{
+	def apply(x:Float) = x.toString
+	def unapply(x:AnyRef):Option[Float] = x match 
+	{
+		case s:String => try { Some(s.toFloat) } catch { case _ => None }
+		case _ => None
+	}
+}
+
+object DoubleValue 
+{
+	def apply(x:Double) = x.toString
+	def unapply(x:AnyRef):Option[Double] = x match 
+	{
+		case s:String => try { Some(s.toDouble) } catch { case _ => None }
+		case _ => None
+	}
+}
+
+object BooleanValue 
+{
+	def apply(x:Boolean) = x.toString
+	def unapply(x:AnyRef):Option[Boolean] = x match 
+	{
+		case s:String => try { Some(s.toBoolean) } catch { case _ => None }
+		case _ => None
+	}
+}
+
+		
