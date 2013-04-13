f6a74c7043e330a9ade1c25406e67c2e1c7edc87
tomer doron <tomer.doron@gmail.com>
2013-04-13 02:36:11 -0700

    cleanup

--- shaft-redis/src/main/scala/com/mishlabs/shaft/repository/redis/RedisStorage.scala
+++ shaft-redis/src/main/scala/com/mishlabs/shaft/repository/redis/RedisStorage.scala
@@ -6,6 +6,6 @@ import scala.collection._
 
 import com.mishlabs.shaft.app.model.Model
 
-protected abstract class RedisStorage[T <: Model] extends Storage[T]
+protected trait RedisStorage[T <: Model] extends Storage[T]
 {
 }
